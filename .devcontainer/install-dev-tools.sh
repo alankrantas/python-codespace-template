fbb29e363f719a9ca7cd73cc077ba77037f5a0c7
Alan Wang <krantas@gmail.com>
2025-04-22 20:59:27 +0800

    Create install-dev-tools.sh

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,6 @@
+apt update
+apt upgrade -y
+apt install -y software-properties-common wget curl python3-dev python3-pip python3-wheel python3-setuptools
+python3 -m pip install --upgrade pip
+pip3 install --user -r .devcontainer/requirements.txt
+apt clean
